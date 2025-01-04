3ebd2617c0d7560a7358ef32d5b0283930cefc28
Radukk2 <raduic2003@gmail.com>
2025-01-04 22:13:58 +0200

    send receive

--- tema2.cpp
+++ tema2.cpp
@@ -22,17 +22,35 @@ struct my_file {
     vector<string> fileParts;
 };
 
+struct tracker_info {
+    vector<int> seeds, peers;
+    set<string> hashes;
+};
+
+
 vector<string> missing_files;
 vector<my_file> files;
 
 void *download_thread_func(void *arg)
 {
     int rank = *(int*) arg;
+
     for (string missing : missing_files) {
-        MPI_Send(missing.c_str(), MAX_FILENAME, MPI_CHAR, rank, 1, MPI_COMM_WORLD);
+        MPI_Send(missing.c_str(), MAX_FILENAME, MPI_CHAR, TRACKER_RANK, 1, MPI_COMM_WORLD);
+        int num_seeds;
+        MPI_Recv(&num_seeds, 1, MPI_INT, TRACKER_RANK, 0, MPI_COMM_WORLD, nullptr);
+        cout <<rank << " "<<  num_seeds << "\n";
+        // for (int i = 0; i < num_seeds; i++) {
+        //     int seed;
+        //     MPI_Recv(&seed, 1, MPI_INT, TRACKER_RANK, 0, MPI_COMM_WORLD, nullptr);
+        // }
     }
+    const char* exitMessage = "EXIT";
+    MPI_Send(exitMessage, MAX_FILENAME, MPI_CHAR, TRACKER_RANK, 1, MPI_COMM_WORLD);
+
     return NULL;
 }
+
 
 void *upload_thread_func(void *arg)
 {
@@ -42,36 +60,46 @@ void *upload_thread_func(void *arg)
 }
 
 void tracker(int numtasks, int rank) {
-    map<string, vector<int>> swarm;
-    vector<int> busy(numtasks + 1, 0);
+    map<string, struct tracker_info> database;
     for (int i = 1; i < numtasks; i++) {
         int num_files_to_read;
         MPI_Recv(&num_files_to_read, 1, MPI_INT, i, MPI_ANY_TAG, MPI_COMM_WORLD, nullptr);
         for (int index = 0; index < num_files_to_read; index++) {
-            int size, num_parts;
+            int size;
             MPI_Recv(&size, 1, MPI_INT, i, 0, MPI_COMM_WORLD, nullptr);
             char* buffer = new char[size + 1];
             MPI_Recv(buffer, size, MPI_CHAR, i, 1, MPI_COMM_WORLD, nullptr);
-            swarm[string(buffer)].push_back(i);
+            database[string(buffer)].seeds.push_back(i);
+            int num_parts;
+            MPI_Recv(&num_parts, 1, MPI_INT, i, 0, MPI_COMM_WORLD, nullptr);
+            for (int idx = 0; idx < num_parts; idx++) {
+                char* buff = new char[HASH_SIZE + 1];
+                MPI_Recv(buff, HASH_SIZE, MPI_CHAR, i, 1, MPI_COMM_WORLD, nullptr);
+                database[string(buffer)].hashes.insert(string(buff));
+            }
         }
     }
-    for (int i = 1; i < numtasks; i++) {
+    for (int i = 1; i < numtasks; i++)
         MPI_Send("ACK", 4, MPI_CHAR, i, 1, MPI_COMM_WORLD);
-    }
-
-    while(true) {
+    
+    int ctr = 0;
+    while (true) {
         MPI_Status status;
         char* buffer = new char[MAX_FILENAME + 1];
         MPI_Recv(buffer, MAX_FILENAME, MPI_CHAR, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &status);
+        buffer[MAX_FILENAME] = '\0';
         string to_be_added(buffer);
-        int my_index, min = 0, index = 0;
-        for (int i : swarm[to_be_added]) {
-            if (min > swarm[to_be_added][index]) {
-                min = swarm[to_be_added][index];
-            }
-            index++;
+        if (to_be_added == "EXIT") { 
+            ctr++;
         }
-        my_index = min;
+        if (ctr == numtasks - 1) {
+            break;
+        }
+        int x = static_cast<int>(database[to_be_added].seeds.size());
+        MPI_Send(&x, 1, MPI_INT, status.MPI_SOURCE, 0, MPI_COMM_WORLD);
+        // for (int seed : database[to_be_added].seeds) {
+        //     MPI_Send(&seed, 1, MPI_INT, status.MPI_SOURCE, 0, MPI_COMM_WORLD);
+        // }
     }
 
 
@@ -96,10 +124,11 @@ void peer(int numtasks, int rank) {
         MPI_Send(file1.file_name.c_str(), size, MPI_CHAR, TRACKER_RANK, 1, MPI_COMM_WORLD);
         int num_parts;
         fin >> num_parts;
+        MPI_Send(&num_parts, 1, MPI_INT, TRACKER_RANK, 0, MPI_COMM_WORLD);
         for (int j = 0; j < num_parts; j++) {
             string part;
             fin >> part;
-            int size1 = static_cast<int>(part.size());
+            MPI_Send(part.c_str(), HASH_SIZE, MPI_CHAR, TRACKER_RANK, 1, MPI_COMM_WORLD);
             file1.fileParts.push_back(part);
         }
         files.push_back(file1);
