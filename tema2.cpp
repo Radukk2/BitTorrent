712f068affd6482b0e20493f2a96ac11e5b48ba3
Radukk2 <raduic2003@gmail.com>
2025-01-07 18:09:02 +0200

    aasad

--- tema2.cpp
+++ tema2.cpp
@@ -46,16 +46,18 @@ void *download_thread_func(void *arg)
     int index = 0;
     int rank = *(int*) arg;
     for (string missing : missing_files) {
-        // cout << "Peer " << rank << " missing file: " << missing << "\n";
+    //     // cout << "Peer " << rank << " missing file: " << missing << "\n";
+        int code = 0;
+        MPI_Send(&code, 1, MPI_INT, TRACKER_RANK, 1, MPI_COMM_WORLD);
         MPI_Send(missing.c_str(), MAX_FILENAME, MPI_CHAR, TRACKER_RANK, 1, MPI_COMM_WORLD);
         int num_seeds;
         MPI_Recv(&num_seeds, 1, MPI_INT, TRACKER_RANK, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        vector<pair<int, int>> effort;
+        vector<int> effort;
         // cout << missing << " Rank:" << rank << " seeds:" << num_seeds << "\n";
         for (int i = 0; i < num_seeds; i++) {
             int seed;
             MPI_Recv(&seed, 1, MPI_INT, TRACKER_RANK, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            effort.push_back(make_pair(seed, 0));
+            effort.push_back(seed);
             // cout << rank << " " << missing << " seed:" << seed<< "\n";
         }
         int num_hashes;
@@ -68,23 +70,28 @@ void *download_thread_func(void *arg)
             wanted_hashes.push_back(string(hash));
             // cout << string(hash) << "\n";
         }
-        // start requesting
+    //     // start requesting
         int total = wanted_hashes.size();
         int i = 0, local = 0;
         my_file new_file;
-        new_file.file_name = missing;
-        // cout << rank << " " << missing << "\n";
-        // cout << total << "\n";
+        files.push_back(new_file);
+        files.back().file_name = missing;
+    //     // cout << rank << " " << missing << "\n";
+    //     // cout << total << "\n";
         while (i < total) {
-            MPI_Send("NRequest", 9, MPI_CHAR, effort[index].first, 4, MPI_COMM_WORLD);
-            MPI_Send(wanted_hashes[i].c_str(), HASH_SIZE, MPI_CHAR, effort[index].first, 3, MPI_COMM_WORLD);
+            if (effort[index] == rank) {
+                index = (index + 1) % effort.size();
+                continue;
+            }
+            MPI_Send("NRequest", 9, MPI_CHAR, effort[index], 3, MPI_COMM_WORLD);
+            MPI_Send(wanted_hashes[i].c_str(), HASH_SIZE, MPI_CHAR, effort[index], 3, MPI_COMM_WORLD);
             int rec;
             char response[15];
-            MPI_Recv(response, 15, MPI_CHAR, effort[index].first, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Recv(response, 15, MPI_CHAR, effort[index], 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             response[14] = '\0';
             if (string(response) == "OK") {
                 // cout << rank << " "<< "found part" << i << "\n";
-                new_file.fileParts.push_back(wanted_hashes.at(i));
+                files.back().fileParts.push_back(wanted_hashes.at(i));
                 i++;
                 local++;
                 // if (i == total)
@@ -95,34 +102,34 @@ void *download_thread_func(void *arg)
                 // cout << "Trying again at index: " << index << "\n";
             }
             if (local == 10) {
-                int num;
-                MPI_Send("Update", MAX_FILENAME, MPI_CHAR, TRACKER_RANK, 1, MPI_COMM_WORLD);
-                MPI_Send(missing.c_str(), MAX_FILENAME, MPI_CHAR, TRACKER_RANK, 4, MPI_COMM_WORLD);
-                cout <<"sent :" << missing << "\n";
-                MPI_Recv(&num, 1, MPI_INT, TRACKER_RANK, 5, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                // int num;
+                // int code = 2;
+                // MPI_Send(&code, 1, MPI_INT, TRACKER_RANK, 1, MPI_COMM_WORLD);
+                // MPI_Send(missing.c_str(), MAX_FILENAME, MPI_CHAR, TRACKER_RANK, 1, MPI_COMM_WORLD);
+                // MPI_Recv(&num, 1, MPI_INT, TRACKER_RANK, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
                 // effort.clear();
-                for (int i = 0; i < num; i++) {
-                    int seed;
-                //     MPI_Recv(&seed, 1, MPI_INT, TRACKER_RANK, 5, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                //     effort.push_back(make_pair(seed, 0));
-                }
-                // fout << rank << " " << " " << missing << " " << i << "\n";
-                local = 0;
+                // // cout << rank << "\n";
+                // for (int i = 0; i < num; i++) {
+                //     int seed;
+                //     MPI_Recv(&seed, 1, MPI_INT, TRACKER_RANK, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                //     effort.push_back(seed);
+                // }
+                // // cout << rank << " " << " " << missing << " " << i << "\n";
+                // local = 0;
             }
         }
-        files.push_back(new_file);
         int idx = 0;
         ofstream out(string("client" + to_string(rank) + "_" + missing));
-        for (string hash : new_file.fileParts)  {
+        for (string hash : files.back().fileParts)  {
             idx++;
-            if (idx != new_file.fileParts.size())
+            if (idx != files.back().fileParts.size())
                 out << hash << "\n";
             else
                 out << hash;
         }
     }
-    const char* exitMessage = "EXIT";
-    MPI_Send(exitMessage, MAX_FILENAME, MPI_CHAR, TRACKER_RANK, 1, MPI_COMM_WORLD);
+    int code = 1;
+    MPI_Send(&code, 1, MPI_INT, TRACKER_RANK, 1, MPI_COMM_WORLD);
     return NULL;
 }
 
@@ -133,26 +140,26 @@ void *upload_thread_func(void *arg)
     while (1) {
         MPI_Status status;
         char comm[9];
-        MPI_Recv(comm, 9, MPI_CHAR, MPI_ANY_SOURCE, 4, MPI_COMM_WORLD, &status);
+        MPI_Recv(comm, 9, MPI_CHAR, MPI_ANY_SOURCE, 3, MPI_COMM_WORLD, &status);
         comm[9] = '\0';
         if (string(comm) == "Shutdown") {
             break;
         }
         char buf[HASH_SIZE+ 1];
-        MPI_Recv(buf, HASH_SIZE, MPI_CHAR, MPI_ANY_SOURCE, 3, MPI_COMM_WORLD, &status);
+        MPI_Recv(buf, HASH_SIZE, MPI_CHAR, status.MPI_SOURCE, 3, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         buf[HASH_SIZE] = '\0';
         string hash(buf);
         bool found = false;
         for (int i = 0; i < files.size(); i++) {
             for (string my_hash : files.at(i).fileParts) {
                 if (my_hash == hash) {
-                    MPI_Send("OK", 15, MPI_CHAR, status.MPI_SOURCE, 2, MPI_COMM_WORLD);
+                    MPI_Send("OK", 15, MPI_CHAR, status.MPI_SOURCE, 1, MPI_COMM_WORLD);
                     found = true;
                 }
             }
         }
         if (not found) {
-            MPI_Send("NOT OK", 15, MPI_CHAR, status.MPI_SOURCE, 2, MPI_COMM_WORLD);
+            MPI_Send("NOT OK", 15, MPI_CHAR, status.MPI_SOURCE, 1, MPI_COMM_WORLD);
         }
     }
     return NULL;
@@ -162,12 +169,12 @@ void tracker(int numtasks, int rank) {
     map<string, struct tracker_info> database;
     for (int i = 1; i < numtasks; i++) {
         int num_files_to_read;
-        MPI_Recv(&num_files_to_read, 1, MPI_INT, i, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(&num_files_to_read, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         for (int index = 0; index < num_files_to_read; index++) {
             int size;
             MPI_Recv(&size, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             char buffer[MAX_FILENAME + 1];
-            MPI_Recv(buffer, MAX_FILENAME, MPI_CHAR, i, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Recv(buffer, MAX_FILENAME, MPI_CHAR, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             buffer[MAX_FILENAME] = '\0';
             database[string(buffer)].seeds.push_back(i);
             int num_parts;
@@ -185,39 +192,46 @@ void tracker(int numtasks, int rank) {
     for (int i = 1; i < numtasks; i++) {
         MPI_Send("ACK", 4, MPI_CHAR, i, 0, MPI_COMM_WORLD);
     }
+    // for (auto it : database) {
+    //     cout << it.first << "\n";
+    //     cout << it.second.seeds.size() << "\n";
+    //     cout << it.second.hashes.size() << "\n\n";
+    // }
     
     int ctr = 0;
     while (true) {
         MPI_Status status;
+        int code;
+        MPI_Recv(&code, 1, MPI_INT, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &status);
+        if (code == 1) { 
+            ctr++;
+            if (ctr == numtasks - 1) {
+                for (int i = 1; i < numtasks; i++) {
+                    MPI_Send("Shutdown", 9, MPI_CHAR, i, 3, MPI_COMM_WORLD);
+                }
+            break;
+        }
+            continue;
+        }
+        if (code == 2) {
+            // char filename[MAX_FILENAME + 1];
+            // MPI_Recv(filename, MAX_FILENAME, MPI_CHAR, status.MPI_SOURCE, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            // filename[MAX_FILENAME] = '\0';
+            // int x = static_cast<int>(database[string(filename)].seeds.size());
+            // MPI_Send(&x, 1, MPI_INT, status.MPI_SOURCE, 1, MPI_COMM_WORLD);
+            // for (int seed : database[string(filename)].seeds) {
+            //     MPI_Send(&seed, 1, MPI_INT, status.MPI_SOURCE, 1, MPI_COMM_WORLD);
+            // }
+            // continue;
+        }
         char buffer[MAX_FILENAME + 1];
         MPI_Recv(buffer, MAX_FILENAME, MPI_CHAR, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &status);
         buffer[MAX_FILENAME] = '\0';
         string to_be_added(buffer);
-        if (to_be_added == "EXIT") { 
-            ctr++;
-        }
-        if (to_be_added == "Update") {
-            char filename[MAX_FILENAME + 1];
-            MPI_Recv(filename, MAX_FILENAME, MPI_CHAR, status.MPI_SOURCE, 4, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            filename[MAX_FILENAME] = '\0';
-            cout <<"received: " <<  string(filename) << "\n";
-            int x = static_cast<int>(database[string(filename)].seeds.size());
-            MPI_Send(&x, 1, MPI_INT, status.MPI_SOURCE, 5, MPI_COMM_WORLD);
-            // for (int seed : database[string(filename)].seeds) {
-            //     MPI_Send(&seed, 1, MPI_INT, status.MPI_SOURCE, 5, MPI_COMM_WORLD);
-            // }
-            continue;
-        }
-        if (ctr == numtasks - 1) {
-            for (int i = 1; i < numtasks; i++) {
-                MPI_Send("Shutdown", 9, MPI_CHAR, i, 4, MPI_COMM_WORLD);
-            }
-            break;
-        }
-        // for (auto it : database) {
-        //     fout << it.first << " ";
-        // }
-        // fout << "\n";
+    //     // for (auto it : database) {
+    //     //     fout << it.first << " ";
+    //     // }
+    //     // fout << "\n";
         int x = static_cast<int>(database[to_be_added].seeds.size());
         MPI_Send(&x, 1, MPI_INT, status.MPI_SOURCE, 1, MPI_COMM_WORLD);
         for (int seed : database[to_be_added].seeds) {
@@ -228,9 +242,10 @@ void tracker(int numtasks, int rank) {
         for (string hash : database[to_be_added].hashes) {
             MPI_Send(hash.c_str(), HASH_SIZE, MPI_CHAR, status.MPI_SOURCE, 1, MPI_COMM_WORLD);
         }
-        if (find(database[to_be_added].seeds.begin(),database[to_be_added].seeds.end(), status.MPI_SOURCE) == database[to_be_added].seeds.end() && to_be_added != "EXIT") {
-            database[to_be_added].seeds.push_back(status.MPI_SOURCE);
-        }
+    // if (find(database[to_be_added].seeds.begin(), database[to_be_added].seeds.end(), status.MPI_SOURCE) == database[to_be_added].seeds.end()) {
+    //    cout << to_be_added << " " << status.MPI_SOURCE << "\n";
+    //    database[to_be_added].seeds.push_back(status.MPI_SOURCE);
+    // }
     }
 }
 
@@ -250,7 +265,7 @@ void peer(int numtasks, int rank) {
         fin >> file1.file_name;
         int size = static_cast<int>(file1.file_name.size());  
         MPI_Send(&size, 1, MPI_INT, TRACKER_RANK, 0, MPI_COMM_WORLD);
-        MPI_Send(file1.file_name.c_str(), size, MPI_CHAR, TRACKER_RANK, 1, MPI_COMM_WORLD);
+        MPI_Send(file1.file_name.c_str(), size, MPI_CHAR, TRACKER_RANK, 0, MPI_COMM_WORLD);
         int num_parts;
         fin >> num_parts;
         MPI_Send(&num_parts, 1, MPI_INT, TRACKER_RANK, 0, MPI_COMM_WORLD);
